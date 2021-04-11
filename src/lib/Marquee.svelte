eaf241c23461d05eb08e6adc80d3fc24b3bae7f4
Tony Ketcham <ketcham@pnw.edu>
2021-04-11 12:56:02 -0500

    🦠 Initialize @keyframe purgatory

---
+++ src/lib/Marquee.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let duration = 15;
+  export let repeat = 9;
+  export let paused = false;
+</script>
+
+<style>
+  .content {
+    width: 100000px;
+  }
+
+  .text {
+    animation-name: animation;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    float: left;
+  }
+
+  .paused .text {
+    animation-play-state: paused;
+  }
+  
+  @keyframes animation {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  }
+</style>
+
+<div style="overflow: hidden;">
+  <div class="content" class:paused={paused}>
+    {#each Array(repeat) as _, i}
+      <div class="text" style="animation-duration: {duration}s">
+        <slot />
+      </div>
+    {/each}
+  </div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Marquee from '$lib/Marquee.svelte';
+
+	const banner = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit'];
+</script>
+
+<main>
+	<h1>Hello @keyframe bug!</h1>
+
+	<Marquee>
+		{#each banner as word}
+			<span style="margin: 0.5rem">{word}</span>
+		{/each}
+	</Marquee>
+
+	<p>Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style style lang="postcss">
+	main {
+		@apply text-center;
+		@apply p-4;
+		@apply mx-auto;
+	}
+
+	h1 {
+		@apply text-red-600;
+		@apply uppercase;
+		@apply text-6xl;
+		@apply font-thin;
+		@apply leading-tight;
+		@apply my-16 mx-auto;
+		@apply max-w-xs;
+	}
+
+	p {
+		@apply max-w-xs;
+		@apply my-8 mx-auto;
+		@apply leading-snug;
+	}
+
+	@screen sm {
+		h1 {
+			@apply max-w-none;
+		}
+
+		p {
+			@apply max-w-none;
+		}
+	}
+</style>
